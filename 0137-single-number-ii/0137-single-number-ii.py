edacbce494b76ba2c8170157b805a9173e3a87cc
Satishbondi@gmail.com <139375976+satishbondi17@users.noreply.github.com>
2025-05-13 10:55:23 +0530

    Time: 0 ms (100%), Space: 13.4 MB (87.27%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        ones, twos = 0, 0
+        for num in nums:
+            ones = (ones ^ num) & ~twos
+            twos = (twos ^ num) & ~ones
+        return ones
+
