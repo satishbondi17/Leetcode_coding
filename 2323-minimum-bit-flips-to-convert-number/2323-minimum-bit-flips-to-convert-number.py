3aed532973ba8f8921064a1c27f691483f474995
Satishbondi@gmail.com <139375976+satishbondi17@users.noreply.github.com>
2025-05-22 16:48:00 +0530

    Time: 0 ms (100%), Space: 12.4 MB (85.17%) - LeetHub

---
+++ 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def minBitFlips(self, start, goal):
+        ans=start^goal
+        count=0
+        for i in range(0,32):
+            if ans&(1<<i)!=0:
+                count+=1
+        return count
+        
