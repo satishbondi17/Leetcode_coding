e726442e4229e02d37964f201c02e217a0e7b645
Satishbondi@gmail.com <139375976+satishbondi17@users.noreply.github.com>
2025-05-27 22:07:25 +0530

    Time: 12 ms (59.43%), Space: 12.6 MB (32.57%) - LeetHub

--- 2571-find-the-pivot-integer/2571-find-the-pivot-integer.py
+++ 2571-find-the-pivot-integer/2571-find-the-pivot-integer.py
@@ -1,12 +1,14 @@
 class Solution(object):
     def pivotInteger(self, n):
-        tot=n*(n+1)//2
-        count=0
-        for i in range(1,n+1):
-            count+=i
-            if count==tot-count+i:
-                return i
+        total = n * (n + 1) // 2
+        left_sum = 0
+        
+        for x in range(1, n + 1):
+            left_sum += x
+            if left_sum == total - left_sum + x:
+                return x
         return -1
+
 
 
 
