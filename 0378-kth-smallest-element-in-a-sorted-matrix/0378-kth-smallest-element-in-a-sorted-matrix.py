11a0b29d6d828219e3f83504ca662b514fb811c4
Satishbondi@gmail.com <139375976+satishbondi17@users.noreply.github.com>
2025-05-31 11:23:47 +0530

    Time: 9 ms (71.43%), Space: 15 MB (43.83%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.py
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.py
@@ -2,8 +2,4 @@ class Solution(object):
     def kthSmallest(self, matrix, k):
         flat = [num for row in matrix for num in row]
         flat.sort()
-        for i in range(0,len(flat)):
-            if i==k-1:
-                return flat[i]
-        return 0
-        
+        return flat[k - 1]
