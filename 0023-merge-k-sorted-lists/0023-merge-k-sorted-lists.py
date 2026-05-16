f79a0aac4b3af27bb89f1b2db8b781700ad126df
Satishbondi@gmail.com <139375976+satishbondi17@users.noreply.github.com>
2026-05-16 09:19:25 +0530

    Time: 4 ms (99.67%), Space: 20.7 MB (5.45%) - LeetHub

---
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def mergeKLists(self, lists):
+        l = []
+        
+        # Traverse all linked lists
+        for i in range(len(lists)):
+            node = lists[i]
+            while node:
+                l.append(node.val)
+                node = node.next
+        
+        # Sort values
+        l.sort()
+        
+        # Create new linked list
+        dummy = ListNode(0)
+        current = dummy
+        
+        for val in l:
+            current.next = ListNode(val)
+            current = current.next
+        
+        return dummy.next
